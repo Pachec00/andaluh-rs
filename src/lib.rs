db47bc871d16264ad269cabc3e83b500b955801c
Daniel García Moreno <dani@danigm.net>
2019-06-21 08:42:02 +0200

    hrule: Keep case for hue and hua replacement

--- src/lib.rs
+++ src/lib.rs
@@ -7,40 +7,35 @@ use failure::Error;
 #[grammar = "andaluh.pest"]
 pub struct AndaluhParser;
 
+fn keep_case(input: &str, case: &str) -> String {
+    input.chars().zip(case.chars())
+        .map(|(i, c)| {
+            match c.is_uppercase() {
+                true => i.to_uppercase().to_string(),
+                false => i.to_lowercase().to_string(),
+            }
+        }).collect::<String>()
+}
+
 pub fn epa() {
-    // let pairs = AndaluhParser::parse(Rule::ident, "a1 b2").unwrap_or_else(|e| panic!("{}", e));
-
-    // Because ident_list is silent, the iterator will contain idents
-    // for pair in pairs {
-        // A pair is a combination of the rule which matched and a span of input
-    //     println!("Rule:    {:?}", pair.as_rule());
-    //     println!("Span:    {:?}", pair.as_span());
-    //     println!("Text:    {}", pair.as_str());
-
-        // A pair can be converted to an iterator of the tokens which make it up:
-    //     for inner_pair in pair.into_inner() {
-    //         match inner_pair.as_rule() {
-    //             Rule::alpha => println!("Letter:  {}", inner_pair.as_str()),
-    //             Rule::digit => println!("Digit:   {}", inner_pair.as_str()),
-    //             _ => unreachable!()
-    //         };
-    //     }
-    // }
 }
 
 pub fn h_rule(input: &str) -> Result<String, Error> {
         let pairs = AndaluhParser::parse(Rule::h, input)?;
-        let mut output: Vec<&str> = vec![];
+        let mut output: Vec<String> = vec![];
 
         for pair in pairs {
             let chunk = match pair.as_rule() {
-                Rule::initial_h => &pair.as_str()[1..],
-                // TODO: mantain the CASE
-                Rule::hue => "gûe",
-                Rule::hua => "gua",
-                Rule::inner_h => &pair.as_str()[1..],
-                Rule::inner_ch => &pair.as_str(),
-                _ => &pair.as_str(),
+                Rule::initial_h => String::from(&pair.as_str()[1..]),
+                Rule::hue => {
+                    keep_case("güe", &pair.as_str())
+                },
+                Rule::hua => {
+                    keep_case("gua", &pair.as_str())
+                },
+                Rule::inner_h => String::from(&pair.as_str()[1..]),
+                Rule::inner_ch => String::from(pair.as_str()),
+                _ => String::from(pair.as_str()),
             };
             output.push(chunk);
         }
@@ -54,8 +49,8 @@ mod tests {
 
     #[test]
     fn test_h_rule() {
-        let input = "hotel HOTEL zanahoria harina chihuahua cacahuete";
-        let expected = "otel OTEL zanaoria arina chiguagua cacagûete";
+        let input = "hotel HOTEL zanahoria harina chihUahua cacaHuEte";
+        let expected = "otel OTEL zanaoria arina chigUagua cacaGüEte";
 
         let output = h_rule(input).expect("Wrong parser");
         assert_eq!(output, expected);
